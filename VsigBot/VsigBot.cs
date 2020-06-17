cc2175063509cb159ab8feceaff298f8b81b390d
VsIG <Valentyn2202@gmail.com>
2020-06-17 23:45:00 +0300

    b(code) code [VsigBot]

---
+++ VsigBot/VsigBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace VsigBot
+{
+	class VsigBot
+	{
+		static void Main()
+		{
+			var botClient = new TelegramBotClient("YOUR_ACCESS_TOKEN_HERE");
+			var me = botClient.GetMeAsync().Result;
+			Console.WriteLine(
+			  $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+			);
+		}
+	}
+}
